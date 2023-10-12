355d7d8ab0a5ef1ac5f7e27b9c35db4082de7591
Vinitkumar Gaikwad <vksg041196@gmail.com>
2023-10-12 20:05:25 +0530

    Update treasure game

--- LoveCalculator/love_calculator.py
+++ LoveCalculator/love_calculator.py
@@ -1,4 +1,4 @@
-#Topics: #CLI_Inputs #StringLowerMethod #StringCountMethod #TypeCasting #if-elif-else #ConditionalOperators #fstring
+#Topics: #CLIInputs #StringLowerMethod #StringCountMethod #TypeCasting #if-elif-else #ConditionalOperators #fstring
 
 name1 = input("What is your name?\n")
 name2 = input("What is your (present/future) partner's name?\n")
--- TreasureGame/treasure_game.py
+++ TreasureGame/treasure_game.py
@@ -1,4 +1,5 @@
 #Topics: #MultilineStrings #If-elif-else #StringLowerMethod #ConditionalOperator
+
 print('''
 *******************************************************************************
           |                   |                  |                     |
@@ -21,31 +22,36 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
 ''')
-print("Welcome to Star's Treasure Island.")
+print("Welcome to Star's Treasure Island!")
 print("Your mission is to find the lost treasure.") 
 
-#Write your code below this line 👇
 print("You are at a cross road. Where do you want to go? Type 'left'/'right'/'forward'/'backward'.\nRemember only one road leads to the treasure.")
 direction = input().lower()
-if direction == "left":
+
+if direction == "left":  
   print("You are smart!")
-  print("You have travelled 5km. But you road is blocked by a river. You can either swim or wait for boat to arrive")
-  print("Type 'wait' or 'swim'")
+  print("You have travelled 5km, but you path is blocked by a river.\nYou can either swim across or wait for boat to arrive")
+  
+  print("What do you choose? Type 'wait' or 'swim'")
   choice = input().lower()
+  
   if choice == "wait":
     print("Your boat has arrived! Hop on.")
     print("Finally you have reached the castle where your treasure is hidden.")
-    print("You see 5 doors. Red, Blue, Green, Yellow, Black.")
-    door_color = input("Which door do you choose? Type 'red'/'blue'/'green'/'yellow'/'black'.\n").lower()
+    print("You see 5 doors with different colors: Red, Blue, Green, Yellow, Black.")
+    
+    print("Which door do you choose? Type 'red'/'blue'/'green'/'yellow'/'black'.")
+    door_color = input().lower()
+    
     if door_color == "yellow":
-      print("You found the treasure. You have won the game!")
+      print("You found the treasure! You have won the game!")
     elif door_color == "red" or door_color == "green":
       print(f"You have been eaten by a {door_color} monster. Game Over.")
     elif door_color == "blue":
-      print("Oh a portal to center of the ocean. You drowned. Game Over.")
+      print("Oh! This a portal to center of the ocean. You drowned! Game Over.")
     else:
-      print("Sucked in to a dark hole. Game Over.")
+      print("You got sucked in to a dark hole! Game Over.")
   else:
     print("The river monster noticed you! Game Over.")
 else:
-  print("I warned you! Game Over.")
+  print("I warned you...\nYou chose the wrong direction and got lost in the maze! Game Over.")
